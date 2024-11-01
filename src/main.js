bc01a241d1d8e25bf1ba7b13cb0fcff9532b77f6
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-11-02 01:06:46 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-const button = document.querySelector("button");
+const shareButton = document.querySelector("button");
 const dialog = document.getElementById("dialog");
+const shareOptions = document.getElementById("shareOptions");
 
-button.addEventListener("click", function () {
-  dialog.style.display = "flex";
+shareButton.addEventListener("click", function () {
+  // Toggle visibility of the dialog
+  if (window.innerWidth >= 640) {
+    if (dialog.style.display === "none" || dialog.style.display === "") {
+      dialog.style.display = "flex";
+    } else {
+      dialog.style.display = "none";
+    }
+  } else {
+    if (shareOptions.style.display === "none" || dialog.style.display === "") {
+      shareOptions.style.display = "flex";
+    } else {
+      shareOptions.style.display = "none";
+    }
+  }
 });
